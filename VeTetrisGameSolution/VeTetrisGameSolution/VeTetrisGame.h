fda9d97af2440b519d31f365ed7794864a60b07e
MihaiZhao <mihai.zhao@gmail.com>
2017-11-14 01:02:36 +0000

    Created Pieces enum

--- VeTetrisGameSolution/VeTetrisGameSolution/VeTetrisGame.h
+++ VeTetrisGameSolution/VeTetrisGameSolution/VeTetrisGame.h
@@ -5,19 +5,30 @@
 #include "Board.h"
 #include "Piece.h"
 
+#define SquarePieceLocation		""
+#define IPieceLocation			""
+#define LPieceLocation			""
+#define LMirroredPieceLocation	""
+#define NPieceLocation			""
+#define NMirroredPieceLocation	""
+#define TPieceLocation			""
+
 class VeTetrisGame :
 	public Game
 {
 private:
-	Piece *m_piece;
+	Piece *m_piece[7];
 	Board *m_board;
 
+	enum Pieces { SquarePiece, IPiece, LPiece, LMirroredPiece, NPiece,NMirroredPiece, TPiece};
 
 public:
 	VeTetrisGame(Board *pBoard, Piece *pPiece);
 	~VeTetrisGame();
 
 	virtual void loop(const float deltaTime) override;
+
+	void loadPices();
 
 };
 
