bfd344f7821ddb88454f82f8c8e03ab2ce58c210
MihaiZhao <mihai.zhao@gmail.com>
2017-11-14 01:00:59 +0000

    Created load pieces funciton

--- VeTetrisGameSolution/VeTetrisGameSolution/VeTetrisGame.cpp
+++ VeTetrisGameSolution/VeTetrisGameSolution/VeTetrisGame.cpp
@@ -6,7 +6,7 @@ VeTetrisGame::VeTetrisGame(Board *pBoard, Piece *pPiece)
 {
 
 	m_board = pBoard;
-	m_piece = pPiece;
+	m_piece[7] = pPiece;
 	
 	m_board->render();
 }
@@ -19,6 +19,24 @@ VeTetrisGame::~VeTetrisGame()
 void VeTetrisGame::loop(const float deltaTime)
 {
 	
+
+
+}
+
+void VeTetrisGame::loadPices()
+{
+	m_piece[SquarePiece]->load("");
+	m_piece[IPiece]->load("");
+	m_piece[LPiece]->load("");
+	m_piece[LMirroredPiece]->load("");
+	m_piece[NPiece]->load("");
+	m_piece[NMirroredPiece]->load("");
+	m_piece[TPiece]->load("");
+
+
+
+
+
 
 
 }
